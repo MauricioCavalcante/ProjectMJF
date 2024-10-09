f25c91934a80c0bf4192f65bc4111c956f8c233e
Mauricio Cavalcante Ribeiro <mauricio.cavalcante@globalhitss.com.br>
2024-10-09 09:21:25 -0300

    Apenas alteração textual no index e lista de espera

--- js/script.js
+++ js/script.js
@@ -1,34 +1,30 @@
 const formularioActionURL = 'https://script.google.com/macros/s/AKfycbzXFjmjL6OeBShPuCqNMMvgjgj07QCDAaCqAwoIom-J3B_kt3vC3oTWhHOi7FsyWLDM/exec';
 const atualizarLinhas = 'https://script.google.com/macros/s/AKfycbx-iqaKTlVJ6kjL8-7kZdgVD2dAzfsioF3WCqdsYN6ucPdVwZhWmJ6iatle2wCxNRlb/exec';
 
-var enviado = false; // Variável para rastrear se o formulário já foi enviado
+var enviado = false;
 
 document.getElementById('meuFormulario').addEventListener('submit', function(event) {
-    event.preventDefault(); // Impede o envio padrão do formulário
+    event.preventDefault(); 
 
-    // Verifica se o formulário já foi enviado
     if (enviado) {
-        return; // Sai da função se o formulário já foi enviado
+        return; 
     }
 
-    // Retira o formulário
     var principal = document.getElementById('principal');
     var final = document.getElementById('final');
     principal.style.display = 'none';
     final.style.display = 'block';
 
-    // Mostra uma mensagem de envio
     var mensagem = document.getElementById('mensagem');
     var contagem = document.getElementById('numeroLinhas');
     mensagem.textContent = 'Enviando formulário...';
     contagem.style.display = 'block';
 
-    // Chama as funções separadas
-    enviarFormulario(); // Função para enviar o formulário
-    atualizarNumeroLinhas(); // Função para atualizar o número de linhas
+    enviarFormulario(); 
+    atualizarNumeroLinhas(); 
 });
 
-// Função para enviar o formulário
+
 function enviarFormulario() {
     var formData = new FormData(document.getElementById('meuFormulario'));
 
@@ -38,11 +34,11 @@ function enviarFormulario() {
     }).then(response => {
         var mensagem = document.getElementById('mensagem');
         if (response.ok) {
-            // Se o envio foi bem-sucedido
+
             mensagem.textContent = 'Obrigada! Seu formulário foi enviado com sucesso, em breve a Mari vai te chamar!';
 
         } else {
-            // Se houve um erro no envio
+
             mensagem.textContent = 'Ocorreu um erro ao enviar o formulário. Por favor, tente novamente.';
             response.text().then(errorMessage => {
                 console.error('Erro ao enviar o formulário:', errorMessage);
@@ -56,16 +52,16 @@ function enviarFormulario() {
         console.error('Erro ao enviar o formulário:', error);
     }).finally(() => {
         enviado = false;    
-        document.getElementById('meuFormulario').reset(); // Limpa o formulário
+        document.getElementById('meuFormulario').reset();
     });
 }
 
-// Função para atualizar o número total de linhas na planilha
+
 function atualizarNumeroLinhas() {
     fetch(atualizarLinhas)
         .then(response => {
             if (response.ok) {
-                return response.json(); // Processa a resposta JSON
+                return response.json(); 
             } else {
                 throw new Error('Erro ao obter número de linhas');
             }
@@ -74,11 +70,11 @@ function atualizarNumeroLinhas() {
             var contagem = document.getElementById('numeroLinhas');
             var contagemMensal = document.getElementById('formularioMensal');
 
-            // Atualiza a contagem exibida na página com o número total de linhas
             contagem.textContent = `Total de formulários enviados até o momento: ${data.totalRowCount}`;
 
-            // Atualiza a contagem de linhas dos últimos 30 dias
-            contagemMensal.textContent = `Total de formulários enviados nos últimos 30 dias: ${data.recentRowCount}`;
+            if (contagemMensal) {
+                contagemMensal.textContent = `Total de formulários enviados nos últimos 30 dias: ${data.recentRowCount}`;
+            }
         })
         .catch(error => {
             console.error('Erro ao obter número de linhas:', error);
