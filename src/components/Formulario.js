5865f89245a46fc37a526349323745864d46641b
Maurício Cavalcante <155476506+MauricioCavalcante@users.noreply.github.com>
2025-04-14 16:37:57 -0300

    Atualização email no form

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,7 +1,7 @@
 import React, { useState, useRef } from "react";
 import PhoneFormatter from "./formulario/PhoneFormatter";
 import NomeInput from "./formulario/NomeInput";
-import EmailInput from './formulario/EmailInput'
+import EmailInput from './formulario/EmailInput';
 import MotivoAtendimento from "./formulario/MotivoAtendimento";
 import TipoConsulta from "./formulario/TipoConsulta";
 import MotivoConsulta from './formulario/MotivoConsulta';
@@ -42,76 +42,7 @@ const Formulario = () => {
       pais: ddi,
     }));
   };
-  const sendConfirmationEmail = async (nome, email) => {
-    const brevoApiKey = process.env.REACT_APP_BREVO_KEY;
 
-    const emailPayload = {
-      sender: {
-        email: "majuferreira.site@gmail.com",
-        name: "MajuFerreira.com",
-      },
-      to: [{ email: email, name: nome }],
-      subject: "Obrigada pela sua solicitação!",
-      htmlContent: `
-        <html>
-          <body>
-            <div style="font-family: Arial, sans-serif; color: #333;">
-              <p style="font-size: 18px; font-weight: bold; color: #794f37; margin-bottom: 20px;">
-                Seu cadastro foi recebido com sucesso!
-              </p>
-              <p>Oi, <strong>${nome}</strong>!</p>
-              <p>
-                Vim aqui te dizer que já recebemos seu cadastro na lista de espera pra
-                ser minha paciente, to ansiosa pra cuidar de você, caso essa seja sua
-                escolha!
-              </p>
-              <p>
-                Lembrando que não sabemos de forma certa em quanto tempo você vai ser
-                chamada, porque isso depende do número de cadastros no mês, número de
-                agendamentos, minha disponibilidade, etc!
-              </p>
-              <p>Se for: 
-                <ul>
-                  <li>Urgente</li>
-                  <li>Inserção de Implanon</li>
-                  <li>Inserção de DIU</li>
-                  <li>Inserção em São Paulo</li>
-                </ul>
-              </p>
-              <p>
-                Sinta-se à vontade para avisar a 
-                <a href="https://www.instagram.com/marisecretariavirtual/" target="_blank">
-                  @marisecretariavirtual
-                </a> no direct do Instagram pra gente tentar um encaixe.
-              </p>
-              <p>Espero você comigo ❤️</p>
-              <p style="font-size: 14px; color: #666; margin-top: 40px;">
-                Este é um e-mail automático. Por favor, não responda.
-              </p>
-            </div>
-          </body>
-        </html>
-      `,
-    };
-
-    try {
-      const response = await axios.post("https://api.brevo.com/v3/smtp/email", emailPayload, {
-        headers: {
-          "api-key": brevoApiKey,
-          "Content-Type": "application/json",
-        },
-      });
-
-      if (response.status === 200) {
-        console.log("E-mail de confirmação enviado com sucesso!");
-      } else {
-        console.error("Falha ao enviar e-mail de confirmação:", response.data);
-      }
-    } catch (error) {
-      console.error("Erro ao enviar o e-mail:", error);
-    }
-  };
-  
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -140,18 +71,71 @@ const Formulario = () => {
       const data = await response.json();
 
       if (response.ok && data.status === "success") {
-  
         document.getElementById("mensagem").textContent =
           "Obrigada! Seu formulário foi enviado com sucesso, em breve a Mari vai te chamar!";
 
-          await sendConfirmationEmail(formData.nome, formData.email);
+        const brevoKey = process.env.REACT_APP_BREVO_KEY;
+        const brevoUrl = "https://api.brevo.com/v3/smtp/email";
 
-        
+        const emailPayload = {
+          sender: { email: "majuferreira.site@gmail.com", name: "MajuFerreira.com" },
+          to: [{ email: formData.email, name: formData.nome }],
+          subject: "Obrigada pela sua solicitação!",
+          htmlContent: `
+            <html>
+              <body>
+                <div style="font-family: Arial, sans-serif; color: #333;">
+                  <p style="font-size: 18px; font-weight: bold; color: #794f37; margin-bottom: 20px;">
+                    Seu cadastro foi recebido com sucesso!
+                  </p>
+                  <p>Oi, <strong>${formData.nome}</strong>!</p>
+                  <p>
+                    Vim aqui te dizer que já recebemos seu cadastro na lista de espera pra
+                    ser minha paciente, tô ansiosa pra cuidar de você, caso essa seja sua
+                    escolha!
+                  </p>
+                  <p>
+                    Lembrando que não sabemos de forma certa em quanto tempo você vai ser
+                    chamada, porque isso depende do número de cadastros no mês, número de
+                    agendamentos, minha disponibilidade, etc!
+                  </p>
+                  <p>Se for: 
+                    <ul>
+                      <li>Urgente</li>
+                      <li>Inserção de Implanon</li>
+                      <li>Inserção de DIU</li>
+                      <li>Inserção em São Paulo</li>
+                    </ul>
+                  </p>
+                  <p>
+                    Sinta-se à vontade para avisar a 
+                    <a href="https://www.instagram.com/marisecretariavirtual/" target="_blank">
+                      @marisecretariavirtual
+                    </a> no direct do Instagram pra gente tentar um encaixe.
+                  </p>
+                  <p>Espero você comigo ❤️</p>
+                  <p style="font-size: 14px; color: #666; margin-top: 40px;">
+                    Este é um e-mail automático. Por favor, não responda.
+                  </p>
+                </div>
+              </body>
+            </html>
+          `,
+        };
+
+        await axios.post(brevoUrl, emailPayload, {
+          headers: {
+            "api-key": brevoKey,
+            "Content-Type": "application/json",
+          },
+        });
+
       } else {
         console.warn("Erro na resposta:", data);
         document.getElementById("mensagem").textContent =
           "Seu formulário foi enviado, mas algo deu errado. Tente novamente.";
       }
+
     } catch (error) {
       console.error("Erro ao enviar o formulário:", error);
       document.getElementById("mensagem").textContent =
